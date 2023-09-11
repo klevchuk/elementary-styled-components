b8e89dea27940abbca49c012f82110414839006b
klevchuk <lemoniklava@gmail.com>
2023-09-11 22:30:37 +0800

    propsiiiiiiiiii

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,16 @@ import React from 'react';
 import './App.css';
 import styled from "styled-components";
 import {Link} from "./components/Link.styled";
-import {Menu} from "./components/Menu.styled";
 import {StyledBtn} from "./components/Button.style";
 
 function App() {
     return (
         <div className="App">
-            <Menu>
-                <ul>
-                    <li><a href="">menu item 1</a></li>
-                    <li><a href="">menu item 2</a></li>
-                    <li><a href="">menu item 3</a></li>
-                </ul>
-            </Menu>
             <Box>
                 {/*<StyledBtn color={"green"} fontSize={"20px"}>Hello</StyledBtn>*/}
                 {/*<StyledBtn color={"red"}>Hello</StyledBtn>*/}
                 {/*<StyledBtn fontSize={"30px"}>Hello</StyledBtn>*/}
+
                 <StyledBtn primary>Hello</StyledBtn>
                 <StyledBtn outlined>Hello</StyledBtn>
             </Box>
--- src/components/Button.style.tsx
+++ src/components/Button.style.tsx
@@ -1,11 +1,11 @@
-import styled from "styled-components";
-import {MyAnimation} from "../styles/animations/Animations";
+import styled, {css} from "styled-components";
 
 type StyledBtnPropsType = {
     color?: string
     fontSize?: string
-    primary: boolean
-    outlined: boolean
+    primary?: boolean
+    outlined?: boolean
+    active?: boolean
 }
 
 export const StyledBtn = styled.button<StyledBtnPropsType>`
@@ -14,14 +14,27 @@ export const StyledBtn = styled.button<StyledBtnPropsType>`
   //background-color: #fb3f78;
   // background-color: ${props => props.color || "#fb3f78"};
   padding: 10px 20px;
-  color: snow;
-  font-size: ${props => props.fontSize};
+  //color: snow;
+  font-size: ${props => props.fontSize || `2rem`};
   font-weight: bold;
 
   &:hover {
     background-color: #4053cc;
   }
 
+  ${props => props.outlined && css<StyledBtnPropsType>`
+border: 2px solid ${props => props.color || "#fb3f78"};
+color: ${props => props.color || "#fb3f78"};
+background-color: transparent;
+    &:hover {
+      border-color: #4053cc;
+      color: #4053cc;
+      background-color: transparent;
+    }
+    
+`}
   
-  
+  ${props => props.primary && css<StyledBtnPropsType>`
+    background-color: ${props => props.color || "#fb3f78"};
+  `}
 `
